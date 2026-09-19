1b218903a4b9d2d572b0e7e5bcd67096b389ad50
Allen Figueroa <figueroa.allen778@gmail.com>
2026-09-19 18:01:54 -0400

    Add files
    Adding updated and additional files such as player movement.

---
+++ Assets/Settings/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D player;
+    public float playerSpeed = 5f;
+    public float horizontalMovement;
+    public float verticalMovement;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    { 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newVelocity = new Vector2(horizontalMovement * playerSpeed, verticalMovement * playerSpeed);
+        player.linearVelocity = newVelocity;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontalMovement = context.ReadValue<Vector2>().x;
+        verticalMovement = context.ReadValue<Vector2>().y;
+    }
+}
